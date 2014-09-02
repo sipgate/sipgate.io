ca02bf9e75ebf1fd2337807f0ae3e51314916249
Benjamin Kluck <github@fuglu.net>
2014-09-02 22:11:58 +0200

    Update mojolicious perl example
    Add basic auth example code
    Add IO::Socket::SSL hint to cpanfile

--- examples/perl/mojolicious/server.pl
+++ examples/perl/mojolicious/server.pl
@@ -2,8 +2,14 @@
 
 use Mojolicious::Lite;
 
+# plugin 'basic_auth';
+
 post '/' => sub {
 	my $c = shift;
+
+	# return if not $c->basic_auth({realm => 'Momcorp',
+	# 				username => 'bender',
+	# 				password => 'rodriguez'});
 
 	my $from = $c->param('from');
 	my $to   = $c->param('to');
