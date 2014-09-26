22bc7d498f904cbb5be250b4d21f89845cd6d66e
Krischan Dickel <dickel@sipgate.de>
2014-09-26 16:30:33 +0200

    Added prolog example

---
+++ examples/prolog/server.pl
@@ -0,0 +1,19 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_client)).
+
+:- http_handler('/', process_request, []).
+
+server :- http_server(http_dispatch, [port(3000)]).
+
+process_request(Request) :-
+	member(method(post), Request), !,
+	http_read_data(Request, Data, []),
+	Data = [From|[To]],
+	print_message(informational, From),
+	print_message(informational, To),
+	answer.
+
+answer :-
+	format('Content-type: text/plain~n~n', []),
+	print('Over and out\n').
