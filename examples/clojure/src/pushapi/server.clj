c6a3895e3c829a391d4ab9588dc8a4ce4335461c
Oliver Wegner <wegner@sipgate.de>
2014-09-26 12:19:38 +0200

    added clojure example

---
+++ examples/clojure/src/pushapi/server.clj
@@ -0,0 +1,23 @@
+;;
+;; This assumes you are using leiningen (http://leiningen.org/)
+;; You can then run this project via
+;; $ lein run
+;;
+;; For complete example (including project file) please look at
+;; https://github.com/sipgate/Push-API/tree/master/examples/clojure
+;;
+
+(ns pushapi.server
+  (:gen-class))
+
+(use 'ring.adapter.jetty
+     'ring.middleware.params)
+
+(defn handler [{params :params}]
+  (println (str "Call from " (params "from") " to " (params "to")))
+  {:status 200 :body "I love the JVM" })
+
+(defn -main
+  [& args]
+  (run-jetty (wrap-params handler) {:port 3000}))
+
