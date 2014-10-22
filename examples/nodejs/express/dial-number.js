7d3bda66539e1085f474878e5bfa1dfb1b0d2c61
Ben Kluck <github@fuglu.net>
2014-10-22 17:09:02 +0200

    Add node.js express dial examples

---
+++ examples/nodejs/express/dial-number.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+var xml = require('xml');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/", function (request, response) {
+	var from = request.body.from;
+	var to = request.body.to;
+
+	console.log("from: " + from);
+	console.log("to: " + to);
+
+	response.set('Content-Type', 'application/xml');
+	response.send(
+		xml({ Response: [
+			{ Dial: [
+				{ Number: "4915799912345" }
+			] },
+		] })
+	);
+});
+
+app.listen(3000);
