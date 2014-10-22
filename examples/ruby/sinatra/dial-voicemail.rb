c1be1df49f09aa8210cbc1dfcbb5ed01679e0b9e
Daniel Schmerwitz <schmerwitz@sipgate.de>
2014-10-22 16:59:50 +0200

    Added ruby examples for reject and dial

---
+++ examples/ruby/sinatra/dial-voicemail.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'builder'
+
+set :port, 3000
+
+post '/' do
+  logger.info "from: #{params[:from]}"
+  logger.info "to: #{params[:to]}"
+
+  headers 'Content-Type' => "application/xml"
+  xml = Builder::XmlMarkup.new(:indent => 2)
+  xml.instruct!
+  xml.Response{xml.Dial{xml.Voicemail}}
+
+end
+
