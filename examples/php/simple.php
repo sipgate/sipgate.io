63c072ca3a00bd08e29a340246f6a0fe1ecfbd3a
danielberlin <daniel.vollbrecht@gmail.com>
2014-11-18 21:03:03 +0100

    Update simple.php
    Added interface for $direction.

--- examples/php/simple.php
+++ examples/php/simple.php
@@ -3,11 +3,12 @@
 // Prepare variables for easier handling
 $fromNumber = $_POST['from'];     // the number of the caller
 $toNumber = $_POST['to'];         // the number on which the call was received on
+$direction = $_POST['direction']; // the direction of the call (either "in" or "out")
 $timestamp = date("d.m.Y H:i:s"); // a timestamp for the log so that calls can be uniquely identified
 
 // build the log row, example:
 // 17.09.2014 10:05:25 - from 4921100000000 to 4921100000000
-$logRow = $timestamp . " - from " . $fromNumber . " to " . $toNumber . PHP_EOL;
+$logRow = $timestamp . " - from " . $fromNumber . " to " . $toNumber . "direction: " . $direction . PHP_EOL;
 
 // append the log row to the callog.txt file, make sure this file is writeable (e.g. create the file and chmod 777 it)
 file_put_contents("callog.txt",$logRow,FILE_APPEND);
