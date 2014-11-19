dbe4e0042f7869ec8a63a7814a5a1308a6fe27b2
Tobias Niepel <github@rollercoder.de>
2014-11-19 09:20:39 +0100

    minor logging change in simple php example

--- examples/php/simple.php
+++ examples/php/simple.php
@@ -7,8 +7,8 @@ $direction = $_POST['direction']; // the direction of the call (either "in" or "
 $timestamp = date("d.m.Y H:i:s"); // a timestamp for the log so that calls can be uniquely identified
 
 // build the log row, example:
-// 17.09.2014 10:05:25 - from 4921100000000 to 4921100000000
-$logRow = $timestamp . " - from " . $fromNumber . " to " . $toNumber . " direction: " . $direction . PHP_EOL;
+// 17.09.2014 10:05:25 - from 4921100000000 to 4921100000000 - direction: in
+$logRow = $timestamp . " - from " . $fromNumber . " to " . $toNumber . " - direction: " . $direction . PHP_EOL;
 
 // append the log row to the callog.txt file, make sure this file is writeable (e.g. create the file and chmod 777 it)
 file_put_contents("callog.txt",$logRow,FILE_APPEND);
