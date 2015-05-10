5391b787f7d7c12c219dc426a7abe82a811ab9a5
danielberlin <danielberlin@users.noreply.github.com>
2015-05-10 22:24:02 +0200

    Update log_call-beginnings.php
    Skript heißt log_call-beginnings.php, loggte aber ohne Berücksichtigung von $_POST['event']. – Nun nur bei newCall.

--- examples/php/log_call-beginnings.php
+++ examples/php/log_call-beginnings.php
@@ -1,16 +1,28 @@
 <?php
+/* Log the beginnings of calls
+ */
 
-// Prepare variables for easier handling
-$fromNumber = $_POST['from'];     // the number of the caller
-$toNumber = $_POST['to'];         // the number on which the call was received on
-$direction = $_POST['direction']; // the direction of the call (either "in" or "out")
-$timestamp = date("d.m.Y H:i:s"); // a timestamp for the log so that calls can be uniquely identified
+$event = $_POST['event'];           // is either "newCall" (beginning) or "hangup" (end)
 
-// build the log row, example:
-// 17.09.2014 10:05:25 - from 4921100000000 to 4921100000000 - direction: in
-$logRow = $timestamp . " - from " . $fromNumber . " to " . $toNumber . " - direction: " . $direction . PHP_EOL;
 
-// append the log row to the callog.txt file, make sure this file is writeable (e.g. create the file and chmod 777 it)
-file_put_contents("callog.txt",$logRow,FILE_APPEND);
+if ($event == 'newCall') {
 
-die("Thanks - here's a motivational squirrel for you! https://www.youtube.com/watch?v=m3d03-sSiBE");
+  // Prepare variables for easier handling
+  $fromNumber = $_POST['from'];     // the number of the caller
+  $toNumber = $_POST['to'];         // the number on which the call was received on
+  $direction = $_POST['direction']; // the direction of the call (either "in" or "out")
+  $callId = $_POST['callId'];       // unique Id of this call
+  $timestamp = date("d.m.Y H:i:s"); // a timestamp for the log so that calls can be uniquely identified
+
+  // build the log row, example:
+  // 23456123 - 17.09.2014 10:05:25 - from 4921100000000 to 4921100000000 - direction: in
+    $logRow = $callId . " - " . $timestamp .
+      " - from " . $fromNumber . " to " . $toNumber .
+      " - direction: " . $direction . PHP_EOL;
+
+  // append the log row to the callog.txt file, make sure this file is writeable (e.g. create the file and chmod 777 it)
+  file_put_contents("callog.txt",$logRow,FILE_APPEND);
+
+  die("Thanks - here's a motivational squirrel for you! https://www.youtube.com/watch?v=m3d03-sSiBE");
+
+}
